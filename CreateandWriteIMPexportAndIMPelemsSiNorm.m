cd09797f8158031390969d8c890bffb20fdc48d7
Dan Ohnemus <42098714+capecodnative@users.noreply.github.com>
2024-07-22 10:25:57 -0400

    Updated heatmaps and moved csv exports to subdirectory

--- CreateandWriteIMPexportAndIMPelemsSiNorm.m
+++ CreateandWriteIMPexportAndIMPelemsSiNorm.m
@@ -1,5 +1,5 @@
 %horizontally merge IMPelems and IMPloc and export as a csv
-writetable(horzcat(IMPloc, IMPelems), 'IMPexport.csv');
+writetable(horzcat(IMPloc, IMPelems), './CSVoutput/IMPexport.csv');
 
 %  Write a separate table of log10 transformed Si-normalized IMPelems
 % (dividing all elements by Si and taking the log10), and name the columns as log10-ElementSi
@@ -7,4 +7,6 @@ IMPelemsSiNorm = IMPelems;
 IMPelemsSiNorm{:, 1:end} = log10(IMPelems{:, 1:end} ./ IMPelems{:, 'Si'});
 IMPelemsSiNorm.Properties.VariableNames(1:end) = strcat('log10-', IMPelems.Properties.VariableNames(1:end), 'Si');
 
-writetable(horzcat(IMPloc,IMPelemsSiNorm), 'IMPelemsSiNorm.csv');
+writetable(horzcat(IMPloc,IMPelemsSiNorm), './CSVoutput/IMPelemsSiNorm.csv');
+
+clear IMPelemsSiNorm
---
+++ plotAllCorrHeatmap_sortedAl.m
@@ -0,0 +1,4 @@
+showCorrHeatmap(AllCorrCoef,ElemNames,1);bwrColormap(50);
+pause;
+saveas(gcf,'./Figures/plotAllCorrHeatmap_sortedAl.fig');
+saveas(gcf,'./Figures/plotAllCorrHeatmap_sortedAl.png');
